2d8240f615ae0e6ce55d2949728d9ed8d914306a
serweryn617 <seweryn.r.1999@gmail.com>
2024-04-11 20:42:03 +0200

    Even better CLI

--- cook/cli.py
+++ cook/cli.py
@@ -15,19 +15,31 @@ def parse_user_args(user_args):
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--recipe_path', default='.', help='Recipe file directory path.')
-    parser.add_argument('options', nargs='*', default={}, help='Project to build followed by user arguments.') #  Uses value of `default_project` if left unspecified, Can be used in recipe file. Format: key=value
+    epilog_text = '\n'.join((
+        'example usage:',
+        '  %(prog)s my_project name=latest -p ./example/ -b local',
+    ))
+
+    parser = argparse.ArgumentParser(
+        description='Build script aggregator and remote executor',
+        epilog=epilog_text,
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+
+    parser.add_argument('-p', '--recipe_path', default='.', help='Path to directory containing `recipe.py` file.')
     parser.add_argument('-b', '--build_server', help='Build server to use. Uses value of `default_build_server` if left unspecified.')
+    parser.add_argument('project', nargs='?', help='Project to build. Uses value of `default_project` if left unspecified.')
+    parser.add_argument('user_args', nargs='*', default=[], help='User arguments. Can be used in recipe file. Format: `key=value`')
 
     args = parser.parse_args()
     recipe_base_path = pathlib.Path.cwd() / args.recipe_path
     build_server = args.build_server
-    if args.options and '=' in args.options[0]:
+    if args.project and '=' in args.project:
         project = None
+        args.user_args.append(args.project)
     else:
-        project = args.options.pop(0)
-    user_args = parse_user_args(args.options)
+        project = args.project
+    user_args = parse_user_args(args.user_args)
 
     recipe = Recipe(recipe_base_path, user_args)
     recipe.load()
