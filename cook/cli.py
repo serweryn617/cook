1fcbcbc2a29d298bb0c547d785faadfe90a692a6
serweryn617 <seweryn.r.1999@gmail.com>
2024-04-23 20:06:21 +0200

    Change cli

--- cook/cli.py
+++ cook/cli.py
@@ -33,14 +33,14 @@ def cli():
         description='Build script aggregator and remote executor', epilog=epilog_text, formatter_class=argparse.RawDescriptionHelpFormatter
     )
 
-    parser.add_argument('-p', '--recipe_path', default='.', help='Path to directory containing `recipe.py` file.')
+    parser.add_argument('-r', '--recipe_path', default='.', help='Path to directory containing `recipe.py` file.')
     parser.add_argument('-b', '--build_server', help='Build server to use. Uses value of `default_build_server` if left unspecified.')
-    parser.add_argument('-r', '--rich_output', action='store_true', help='Use Rich to format output.')
+    parser.add_argument('-f', '--format', action='store_true', help='Format output using Rich.')
     parser.add_argument('-t', '--targets', action='store_true', help='List available projects.')
     parser.add_argument('-d', '--dry', action='store_true', help='Dry run.')
     parser.add_argument('-q', '--quiet', action='store_true', help='Suppress stdout.')
-    parser.add_argument('project', nargs='?', help='Project to build. Uses value of `default_project` if left unspecified.')
-    parser.add_argument('user_args', nargs='*', default=[], help='User arguments. Can be used in recipe file. Format: `key=value`')
+    parser.add_argument('-u', '--user_args', nargs='*', default=[], help='User arguments. Can be used in recipe file. Format: `key=value`')
+    parser.add_argument('-p', '--project', default=None, help='Project to build. Uses value of `default_project` if left unspecified.')
 
     # TODO: add user flag args
     # TODO: use better user args, e.g. --name latest
@@ -48,13 +48,9 @@ def cli():
     args = parser.parse_args()
     recipe_base_path = (pathlib.Path.cwd() / args.recipe_path).resolve()
     build_server = args.build_server
-    if args.project and '=' in args.project:
-        project = None
-        args.user_args.insert(0, args.project)
-    else:
-        project = args.project
+    project = args.project
     settings.args.update(parse_user_args(args.user_args))
-    rich_output = args.rich_output
+    rich_output = args.format
     quiet = args.quiet
 
     list_targets = args.targets
