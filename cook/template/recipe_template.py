d51f44361f91d2ae886dc056f4902a717805ae9e
serweryn617 <seweryn.r.1999@gmail.com>
2024-08-04 10:56:49 +0200

    Update template

--- cook/template/recipe_template.py
+++ cook/template/recipe_template.py
@@ -1,7 +1,7 @@
 TEMPLATE = '''# Cooking recipe, checkout Cook at https://github.com/serweryn617/cook
 
 default_build_server = 'local'
-default_project = 'build'
+# default_project = 'build'
 
 build_servers = [
     'local',
@@ -18,12 +18,12 @@ projects['run_all'] = {
 
 projects['clean'] = (
     # 'rm -rf build',
-    'echo "running: rm -rf build"',
+    'echo "cleaning build directory"',
 )
 
 projects['build'] = (
     # 'mkdir -p build',
-    'echo "running: mkdir -p build"',
+    'echo "creating build directory"',
     # ('build', 'cmake ..'),
     # ('build', 'cmake --build .'),
     'echo "running cmake in build directory"',
