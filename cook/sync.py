6ef0687d85cd7d88174da86685d27e5e0fbc3a8e
serweryn617 <seweryn.r.1999@gmail.com>
2024-08-04 10:36:48 +0200

    Simplify directory sync

--- cook/sync.py
+++ cook/sync.py
@@ -8,26 +8,6 @@ class RsyncItem:
         self.path = path
 
 
-class SyncDirectory(RsyncItem):
-    def __init__(self, path: str = ''):
-        self.path = path
-
-    def parse(self, src_hostname: str = '', src_path: str = '', dst_hostname: str = '', dst_path: str = '') -> (str, str):
-        # TODO: add support for absolute paths
-        src = Path(src_path) / self.path
-        # Add slash at the end to treat source as directory
-        src = src.as_posix() + '/'
-        if src_hostname:
-            src = src_hostname + ':' + src
-
-        dst = Path(dst_path) / self.path
-        dst = dst.as_posix()
-        if dst_hostname:
-            dst = dst_hostname + ':' + dst
-
-        return src, dst
-
-
 class SyncFile(RsyncItem):
     def parse(self, src_hostname: str = '', src_path: str = '', dst_hostname: str = '', dst_path: str = '') -> (str, str):
         # TODO: add support for absolute paths
@@ -42,6 +22,17 @@ class SyncFile(RsyncItem):
             dst = dst_hostname + ':' + dst
 
         return src, dst
+
+
+class SyncDirectory(SyncFile):
+    def __init__(self, path: str = ''):
+        self.path = path
+
+    def parse(self, src_hostname: str = '', src_path: str = '', dst_hostname: str = '', dst_path: str = '') -> (str, str):
+        src, dst = super().parse(src_hostname, src_path, dst_hostname, dst_path)
+
+        # Add slash at the end to treat source as directory
+        return src + '/', dst
 
 
 class SyncExclude(RsyncItem):
