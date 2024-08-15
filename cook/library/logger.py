c85d04f743e1d8d8d1fc57e18d30a7c8f737f2ce
serweryn617 <seweryn.r.1999@gmail.com>
2024-08-15 16:34:45 +0200

    Further simplify logger

--- cook/library/logger.py
+++ cook/library/logger.py
@@ -1,36 +1,29 @@
 from .terminal import EscapeCodes
 
+GREEN = 0x00, 0xCC, 0x52
+PURPLE = 0xD8, 0x49, 0xFF
+ORANGE = 0xFF, 0x88, 0x00
+RED = 0xFF, 0x00, 0x00
 
-class Logger:
-    GREEN = 0x00, 0xCC, 0x52
-    PURPLE = 0xD8, 0x49, 0xFF
-    ORANGE = 0xFF, 0x88, 0x00
-    RED = 0xFF, 0x00, 0x00
-
-    theme = {
-        'log': GREEN,
-        'info': PURPLE,
-        'warning': ORANGE,
-        'error': RED,
-    }
-
-    def __init__(self):
-        self.formatted = True
-
-    def __call__(self, message, style=None, bold=False, internal=True):
-        message = str(message)
-
-        if internal:
-            message = '==> ' + message
-
-        if bold:
-            message = EscapeCodes.bold + message + EscapeCodes.reset
-
-        if style:
-            color = EscapeCodes.fg(*self.theme[style])
-            message = color + message + EscapeCodes.reset
-
-        print(message)
+THEME = {
+    'log': GREEN,
+    'info': PURPLE,
+    'warning': ORANGE,
+    'error': RED,
+}
 
 
-log = Logger()
+def log(message, style=None, bold=False, internal=True):
+    message = str(message)
+
+    if internal:
+        message = '==> ' + message
+
+    if bold:
+        message = EscapeCodes.bold + message + EscapeCodes.reset
+
+    if style:
+        color = EscapeCodes.fg(*THEME[style])
+        message = color + message + EscapeCodes.reset
+
+    print(message)
